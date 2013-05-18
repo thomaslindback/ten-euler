0f9c19e90f6da244effd65d5ad82a7f1cd924243
thomaslindback <thomas.lindback@gmail.com>
2013-05-18 17:42:36 +0300

    added scala and rust

---
+++ e1/e1.scala
@@ -0,0 +1,36 @@
+package org.teel
+
+object E1 {
+
+  def e1_match(n: Int, acc: Int): Int = n match {
+    case 0 => acc
+    case _ => e1_match(n - 1, if (n % 5 == 0 || n % 3 == 0) acc + n else acc)
+  }
+
+  def e1_explicit(n: Int): Int = {
+    var sum = 0
+    for (i <- 0 until n) {
+      if (i % 5 == 0 || i % 3 == 0) {
+        sum += i;
+      }
+    }
+    sum
+  }
+
+  def e1_fold_1(n: Int): Int = {
+    def list = for (i <- 0 until n) yield i
+    list.foldLeft(0) { (sum, i) => if (i % 5 == 0 || i % 3 == 0) sum + i else sum }    
+  }
+  
+  def e1_fold_2(n: Int): Int = {
+    def list = for ( i <- 0 until n ; if (i % 5 == 0 || i % 3 == 0) ) yield i
+    list.foldLeft(0) {_+_}
+  }
+
+  def main(args: Array[String]) {
+    println(e1_explicit(1000))
+    println(e1_match(999, 0))
+    println(e1_fold_1(1000))
+    println(e1_fold_2(1000))
+  }
+}
