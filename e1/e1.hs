55d222949689f049fd5315020f8b9cfc9a508002
Thomas Lindbäck <thomas.lindback@gmail.com>
2013-05-26 21:44:51 +0300

    change directory structure

--- e1/e1.hs
+++
@@ -1,6 +0,0 @@
--- Project Euler 1
-
--- fkn
-e1 n = sum [ x | x <- [1..n], x `mod` 5 == 0 || x `mod` 3 == 0]
--- hard coded
-e1_999 = sum [ x | x <- [1..999], mod x 5 == 0 || mod x 3 == 0]
--- e4/e4.py
+++
@@ -1,9 +0,0 @@
-# Project Euler 4
-
-import itertools
-
-def isPalindrome(n):
-	return str(n) == str(n)[::-1]
-
-print max([ a*b for (a,b) in itertools.permutations(range(100, 1000), 2) if isPalindrome(a*b)])
-
